b57323c63afc57ee0d75c2f21f3863c07ab69723
anu1905 <anu1905@gmail.com>
2018-07-09 15:56:54 +0200

    Hej from Sweden

--- sketch.js
+++ sketch.js
@@ -5,10 +5,14 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background("green");
+  background('#FFFFFF');
   noStroke();
-  fill(100);
-  ellipse(mouseX,mouseY,40,40);
+  fill("blue");
+  rect(width/2, height/2, 200, 120);
+  fill("yellow");
+  rect(340, height/2, 20, 120);
+  rect(width/2, 240, 200, 20);
   fill("red");
-  text("Hello World", mouseX, mouseY);
+  textSize(20);
+  text("Hej hej, love from Sweden", 280, 360);
 }
